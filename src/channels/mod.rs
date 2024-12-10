d0bba00f54458ccaeb5667918843b25790d57d30
Ryan Baggs <ryanbaggs7@gmail.com>
2024-12-10 16:25:43 -0600

    Test program
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/channels/mod.rs
+++ src/channels/mod.rs
@@ -1,3 +1,3 @@
-mod mutex_based;
-mod unsafe_oneshot;
-mod safer_oneshot;
+pub mod mutex_based;
+pub mod unsafe_oneshot;
+pub mod safer_oneshot;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod channels;
 
+use std::thread;
+use channels::safer_oneshot;
+
 fn main() {
-    println!("Hello, world!");
+    let channel = safer_oneshot::Channel::new();
+    let t = thread::current();
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("Hello World!");
+            t.unpark();
+        });
+
+        while !channel.is_ready() {
+            thread::park();
+        }
+
+        assert_eq!(channel.receive(), "Hello World!");
+    });
 }

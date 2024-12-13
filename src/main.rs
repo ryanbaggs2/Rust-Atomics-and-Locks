55e34ab95f8bae3cbc5e20241df717fea738b1c2
Ryan Baggs <ryanbaggs7@gmail.com>
2024-12-12 22:11:59 -0600

    Implementation catches multiple calls to send/receive at compile time
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod channels;
 
 use std::thread;
 use channels::safer_oneshot;
+use channels::compile_time_oneshot;
 
 fn main() {
     let channel = safer_oneshot::Channel::new();
@@ -17,5 +18,20 @@ fn main() {
         }
 
         assert_eq!(channel.receive(), "Hello World!");
+    });
+
+    thread::scope(|s| {
+        let (sender, receiver) = compile_time_oneshot::channel();
+
+        s.spawn(|| {
+            sender.send("Hello World!");
+            t.unpark()
+        });
+
+        while !receiver.is_ready() {
+            thread::park();
+        }
+        
+        assert_eq!(receiver.receive(), "Hello World!");
     });
 }
